b949b8dc579c77034ff49d6c645cb574b7fe6622
mana05 <mana@realsoft.co.jp>
2024-08-28 17:08:01 +0900

    スライドショーを修正

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ function popup() {
 }
 
 // 画像のスライドショー
-const images = [
-    { src: "images/big_plant.jpg", title: "成長したガジュマル" },
-    { src: "images/mini_plant.jpg", title: "小さかった頃のガジュマル" },
-    { src: "images/ramen.jpg", title: "辛いラーメン" }
-]
+// const images = [
+//     { src: "images/big_plant.jpg", title: "成長したガジュマル" },
+//     { src: "images/mini_plant.jpg", title: "小さかった頃のガジュマル" },
+//     { src: "images/ramen.jpg", title: "辛いラーメン" }
+// ]
+const images =["images/big_plant.jpg", "images/mini_plant.jpg", "images/ramen.jpg"]
 let num = 0;
 
 function slideshow() {
     num = (num + 1) % images.length;
     // console.log(num);
-    document.getElementById("slide-image").src = images[num].src;
-    document.getElementById("slide-title").innerHTML = images[num].title;
+    document.getElementById("slide-image").src = images[num];
+    // document.getElementById("slide-image").src = images[num].src;
+    // document.getElementById("slide-title").innerHTML = images[num].title;
 }
 setInterval(slideshow, 2000);
 
